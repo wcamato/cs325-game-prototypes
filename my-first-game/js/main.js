ecc5d1a23f6bd8c0c53e3710a6903ac8f1aedb80
wcamato <78327310+wcamato@users.noreply.github.com>
2021-02-02 17:59:26 -0500

    made some changes

--- my-first-game/js/main.js
+++ my-first-game/js/main.js
@@ -21,7 +21,7 @@ class MyScene extends Phaser.Scene {
     
     preload() {
         // Load an image and call it 'logo'.
-        this.load.image( 'logo', 'assets/Jeter.png' );
+        this.load.image( 'logo', 'assets/Jeter.PNG' );
     }
     
     create() {
